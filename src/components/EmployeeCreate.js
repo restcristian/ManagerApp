f82e3a3db29e99645df84ec1c69505f9cc3ed9ab
1526853 <c.a.restituyonunez@wlv.ac.uk>
2018-11-26 23:11:50 -0400

    Allowed CardSection to take extra styling.

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Picker, Text, StyleSheet } from 'react-native';
 import { Card, CardSection, Input, Button } from '../components/common';
 import { employeeUpdate } from '../actions';
 import { connect } from 'react-redux';
@@ -23,7 +24,22 @@ class EmployeeCreate extends Component {
                         onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text })}
                     />
                 </CardSection>
-                <CardSection></CardSection>
+                <CardSection style={{ flexDirection: 'row' }}>
+                    <Text style={styles.pickerTextStyle}>Shift</Text>
+                    <Picker
+                        style={{ flex: 1}}
+                        selectedValue={this.props.shift}
+                        onValueChange={text => this.props.employeeUpdate({ prop: 'shift', value: text })}
+                    >
+                        <Picker.Item label="Monday" value="Monday" />
+                        <Picker.Item label="Tuesday" value="Tuesday" />
+                        <Picker.Item label="Wednesday" value="Wednesday" />
+                        <Picker.Item label="Thursday" value="Thursday" />
+                        <Picker.Item label="Friday" value="Friday" />
+                        <Picker.Item label="Saturday" value="Saturday" />
+                        <Picker.Item label="Sunday" value="Sunday" />
+                    </Picker>
+                </CardSection>
                 <CardSection>
                     <Button>Create</Button>
                 </CardSection>
@@ -31,7 +47,12 @@ class EmployeeCreate extends Component {
         );
     }
 }
-
+const styles = StyleSheet.create({
+    pickerTextStyle: {
+        fontSize: 18,
+        paddingLeft: 20
+    }
+});
 const mapStateToProps = state => {
     const { name, phone, shift } = state.employeeForm;
     return {
