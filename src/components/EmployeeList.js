a49bd31cb7b1d3af26748193180b44f7fd9f5613
1526853 <c.a.restituyonunez@wlv.ac.uk>
2018-11-27 23:18:11 -0400

    Rendered employees list as an array.

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import { employeesFetch } from '../actions';
 import { connect } from 'react-redux';
 
@@ -8,22 +8,45 @@ class EmployeeList extends Component {
     componentDidMount() {
         this.props.employeesFetch();
     }
+    getEmployeeList() {
+
+    }
+    renderEmployee = ({ item }) => {
+        return <Text>Hello Employee</Text>;
+    }
+    
     render() {
+        // console.log('employeeprops', this.props.employees)
         return (
-            <View>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-            </View>
+            <FlatList
+                data={this.props.employees}
+                renderItem={this.renderEmployee}
+                keyExtractor={item => item.id}
+            />
+
 
         );
     }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
 
+    const { employees } = state;
+    const keys = Object.keys(employees);
+
+    let employeeList = [];
+
+    keys.forEach(id => {
+        const tempEmployee = {
+            id: id,
+            ...employees[id]
+        };
+        employeeList.push(tempEmployee);
+    });
+
+    return {
+        employees: employeeList
+    };
 };
 
 export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
