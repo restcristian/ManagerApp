b18301416942396db9fa0957d447f84e3b2694b0
1526853 <c.a.restituyonunez@wlv.ac.uk>
2018-11-28 02:02:03 -0400

    Added Library and implementation for texting.

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,6 +3,7 @@ import { Card, CardSection, Button } from './common';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
+import Communications from 'react-native-communications';
 
 class EmployeeEdit extends Component {
     componentDidMount() {
@@ -16,15 +17,21 @@ class EmployeeEdit extends Component {
     onButtonPress = () => {
         const { name, phone, shift } = this.props;
         const { employee } = this.props;
-        
+
         this.props.employeeSave({
             name,
             phone,
             shift,
-            id:employee.id
+            id: employee.id
         });
     }
 
+    onTextPress = () => {
+        const { phone, shift } = this.props;
+
+        Communications.text(phone, `Your upcoming shift is on ${shift}`);
+
+    }
 
     render() {
         return (
@@ -33,6 +40,11 @@ class EmployeeEdit extends Component {
                 <CardSection>
                     <Button onPress={this.onButtonPress}>
                         Save Changes
+                    </Button>
+                </CardSection>
+                <CardSection>
+                    <Button onPress={this.onTextPress}>
+                        Text Schedule
                     </Button>
                 </CardSection>
             </Card>
